304947cfa08360c45b4b834e15f262a582514539
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-10-21 12:23:44 +0800

    Add SQLite and Room Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,19 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.8.0")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.0")
+    annotationProcessor("androidx.room:room-compiler:2.6.0")
+    androidTestImplementation("androidx.room:room-testing:2.6.0")
+
+    //Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.6.2")
+
+    //UI
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.android.material:material:1.10.0")
 }
