5260763aed1cd476a94ea078f323640181361422
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-10-21 14:35:11 +0800

    Implement repository

---
+++ app/src/main/java/com/mobdeve/s15/taboo/DataRepository.java
@@ -0,0 +1,50 @@
+package com.mobdeve.s15.taboo;
+
+import android.app.Application;
+import android.widget.Switch;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+class DataRepository {
+
+    private DAO mTabooDao;
+    private LiveData<List<Treasure>> mTreasury;
+    private LiveData<PlayerData> mPlayerData;
+
+    DataRepository(Application application) {
+        TabooDatabase db = TabooDatabase.getDatabase(application);
+        mTabooDao = db.TabooDao();
+        mPlayerData = mTabooDao.getPlayerData();
+        mTreasury = mTabooDao.getTreasury();
+    }
+
+    LiveData<List<Treasure>> getTreasury() {
+        return mTreasury;
+    }
+
+    LiveData<PlayerData> getPlayerData(){
+        return mPlayerData;
+    }
+
+    void updatePlayer(PlayerData playerData) {
+        TabooDatabase.databaseWriteExecutor.execute(() -> {
+            mTabooDao.updatePlayer(playerData);
+        });
+    }
+    void updateTreasury(Treasure treasure) {
+        TabooDatabase.databaseWriteExecutor.execute(() -> {
+            mTabooDao.updateTreasury(treasure);
+        });
+    }
+
+    void deleteData() {
+        TabooDatabase.databaseWriteExecutor.execute(() -> {
+            mTabooDao.deletePlayer();
+        });
+        TabooDatabase.databaseWriteExecutor.execute(() -> {
+            mTabooDao.deleteTreasures();
+        });
+    }
+}
