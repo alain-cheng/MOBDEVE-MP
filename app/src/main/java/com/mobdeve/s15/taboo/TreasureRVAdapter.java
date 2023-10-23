a291ef4ab9ac1a26eb48c8c293bb23710b7fb840
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-10-23 15:11:01 +0800

    Added click anim and made whole card clickable

--- app/src/main/java/com/mobdeve/s15/taboo/TreasureRVAdapter.java
+++ app/src/main/java/com/mobdeve/s15/taboo/TreasureRVAdapter.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -18,6 +20,7 @@ import java.util.List;
 public class TreasureRVAdapter extends RecyclerView.Adapter<TreasureRVAdapter.MyViewHolder> {
     Context context;
     List<Treasure> treasures;
+    private final AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.7F); //For button effects, should probably be replaced by something else
 
     public TreasureRVAdapter(Context context, List<Treasure> treasures) {
         this.context = context;
@@ -34,13 +37,15 @@ public class TreasureRVAdapter extends RecyclerView.Adapter<TreasureRVAdapter.My
 
     @Override
     public void onBindViewHolder(@NonNull TreasureRVAdapter.MyViewHolder holder, int position) {
+        String name = treasures.get(position).getName();
+
         holder.imageView.setImageResource(treasures.get(position).getImageid());
-        holder.tvName.setText(treasures.get(position).getName());
+        holder.tvName.setText(name);
         holder.tvBonus.setText(treasures.get(position).getItemBonus());
 
-
-        holder.imageView.setOnClickListener(new View.OnClickListener() {
+        holder.itemCard.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                v.startAnimation(buttonClick);
                 Intent intent = new Intent(context, TreasureView.class);
                 context.startActivity(intent);
             }
@@ -57,9 +62,11 @@ public class TreasureRVAdapter extends RecyclerView.Adapter<TreasureRVAdapter.My
 
         ImageView imageView;
         TextView tvName, tvBonus;
+        CardView itemCard;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            itemCard = itemView.findViewById(R.id.item_card);
             imageView = itemView.findViewById(R.id.item_thumbnail_iv);
             tvName = itemView.findViewById(R.id.item_name_tv);
             tvBonus = itemView.findViewById(R.id.item_bonus_tv);
