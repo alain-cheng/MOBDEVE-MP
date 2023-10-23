a669b12465618570600cbfb469dd98f9b4a4eab9
akn5 <chengp.alain@gmail.com>
2023-10-23 12:15:41 +0800

    Update: RecyclerView for Collection activity view.

---
+++ app/src/main/java/com/mobdeve/s15/taboo/TreasureModelTemp.java
@@ -0,0 +1,28 @@
+package com.mobdeve.s15.taboo;
+
+// Placeholder Class only meant for Prototype!
+// Delete when no longer in use.
+// Used by the collection recycler view.
+
+public class TreasureModelTemp {
+    int image;
+    String itemName, itemBonus;
+
+    public TreasureModelTemp(int image, String itemName, String itemBonus) {
+        this.image = image;
+        this.itemName = itemName;
+        this.itemBonus = itemBonus;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemBonus() {
+        return itemBonus;
+    }
+}
