5a10d492cb4b29c12a33dc777868ef731a350244
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-10-27 17:13:17 +0800

    Login layout

--- app/src/main/java/com/mobdeve/s15/taboo/Setting.java
+++ app/src/main/java/com/mobdeve/s15/taboo/Setting.java
@@ -31,13 +31,39 @@ public class Setting extends AppCompatActivity {
         //Listeners
         binding.settingsBackIbtn.setOnClickListener(this::backListener);
         binding.erasebutton.setOnClickListener(this::eraseListener);
+        binding.loginbutton.setOnClickListener(this::loginListener);
+        binding.logoutbutton.setOnClickListener(this::logoutListener);
     }
 
     private void backListener(View v){
         v.startAnimation(buttonClick);
         finish();
     }
+    private void loginListener(View v){
+        v.startAnimation(buttonClick);
+        //TODO: Add a popup that will ask for login details to sign up or login.
+        //TODO: Add a function in DataViewModel that will contact server to check or add login info
+        //TODO: Connect to db to store login details in a table if data matches the server, logout empties the table.
+        //Test Only Delete later
+        binding.nameguestView.setImageResource(R.drawable.itemboxname_sample);
+        binding.loginbutton.setImageResource(R.drawable.group_718email_sample);
+        binding.loginbutton.setClickable(false); //To prevent animation from playing or logging in twice
+        binding.logoutbutton.setVisibility(View.VISIBLE);
+
+    }
+    private void logoutListener(View v){
+        v.startAnimation(buttonClick);
+        //TODO: Add a popup that will ask if user is sure.
+        //TODO: logout should delete data in table account
+        //Test Only Delete later
+        binding.nameguestView.setImageResource(R.drawable.itemboxname_guest);
+        binding.loginbutton.setImageResource(R.drawable.group_716login_button);
+        binding.loginbutton.setClickable(true);
+        binding.logoutbutton.setVisibility(View.GONE);
+
+    }
     private void eraseListener(View v){
+        //TODO: Add a popup that will ask if user is sure.
         v.startAnimation(buttonClick);
         mDataViewModel.deleteData();
         finish();
