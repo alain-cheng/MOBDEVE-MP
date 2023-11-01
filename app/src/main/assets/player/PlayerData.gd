7f96c1b0b0ae0bc88949166fd988c555efb57a1c
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-11-01 22:07:44 +0800

    Godot can receive and send data via JSON

--- app/src/main/assets/player/PlayerData.gd
+++ app/src/main/assets/player/PlayerData.gd
@@ -15,3 +15,23 @@ var taboo
 var tabooBonus
 var luck
 var bountyBonus
+
+func initData():
+	var file = FileAccess.get_file_as_string("user://player_data.json")
+	var player_dict = JSON.parse_string(file)
+	
+	#Assign initial data
+	initHealth = player_dict.get("health")
+	initBounty = player_dict.get("bounty")
+	initTaboo = player_dict.get("taboo")
+	initTabooBonus = player_dict.get("tabooBonus")
+	initLuck = player_dict.get("luck")
+	initBountyBonus = player_dict.get("bountyBonus")
+	
+	#Copy data to manipulable variables
+	health = initHealth
+	bounty = initBounty
+	taboo = initTaboo
+	tabooBonus = initTabooBonus
+	luck = initLuck
+	bountyBonus = initBountyBonus
