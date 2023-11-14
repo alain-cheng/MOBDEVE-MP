310ec6450f5811e012850773ae42920c8a083552
akn5 <chengp.alain@gmail.com>
2023-11-14 18:15:25 +0800

    Refactor: Parent class for bullet-type Projectiles

--- app/src/main/assets/traps/Projectiles/fire_projectile_2.gd
+++ app/src/main/assets/traps/Projectiles/fire_projectile_2.gd
@@ -1,34 +1,6 @@
-extends Area2D
-
-@onready var duration = $Duration
-var damage = 1
-var SPEED = 500
-signal damage_taken(damage)
+extends Projectile1
 
 func _ready():
+	damage = 1
+	SPEED = 500
 	duration.start()
-
-
-func _physics_process(delta):
-	var direction = Vector2.LEFT.rotated(rotation)
-	global_position += SPEED * direction * delta
-
-
-func destroy():
-	queue_free()
-
-
-func _on_area_entered(area):
-	if area.name == "PlayerHurtbox":
-		damage_taken.emit(damage)
-		destroy()
-	#if body.is_in_group("Mob") == true:
-		#print("Collided with a Mob")
-		#destroy()
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
-
-
-func _on_duration_timeout():
-	queue_free()
