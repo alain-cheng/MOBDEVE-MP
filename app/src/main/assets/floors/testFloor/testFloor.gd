f1a7c32f75d6f6b5a22264f38ef330d6742b9498
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-11-14 23:28:33 +0800

    Pitfall implementation

--- app/src/main/assets/floors/testFloor/testFloor.gd
+++ app/src/main/assets/floors/testFloor/testFloor.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var gDragons = [get_node("GoldenDragon"), get_node("GoldenDragon2")]
 @onready var mananaggal = [get_node("Manananggal")]
 @onready var kapre = [get_node("Kapre")]
+@onready var pitfalls = [get_node("Pitfall")]
 @onready var player = get_node("Player")
 
 # Called when the node enters the scene tree for the first time.
@@ -19,14 +20,15 @@ func _ready():
 		m.player = player
 	for k in kapre:
 		k.player = player
+	for p in pitfalls:
+		p.fallen_down.connect(player.ive_fallen)
 	for spike in spikes1:
 		spike.damage_taken.connect(player.on_damage_taken)
 		spike.timer.wait_time = 2.0
 		spike.timer.one_shot = false
 		spike.timer.start()
 
-
-func _on_end_point_change_floor():
+func _on_change_floor():
 	#Move to another scene randomly
 	PlayerData.lastFloor = true
 	get_tree().change_scene_to_file("res://floors/testFloor/testFloor2.tscn")
