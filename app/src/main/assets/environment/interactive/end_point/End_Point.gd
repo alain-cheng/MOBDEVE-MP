1c43ebee907e6bd9c5ddd48bf88ef8bc626720d3
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-11-17 12:52:33 +0800

    Modified the sprites and collision of endpoint and pitfall

--- app/src/main/assets/environment/interactive/end_point/End_Point.gd
+++ app/src/main/assets/environment/interactive/end_point/End_Point.gd
@@ -1,17 +1,14 @@
 extends Area2D
 
 @onready var flooranim = get_node("FloorSprite")
-@onready var floatanim = get_node("FloatSprite")
 signal changeFloor
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if(!PlayerData.lastFloor):
 		flooranim.play("nextfloor")
-		floatanim.play("nextfloor")
 	else:
 		flooranim.play("treasure")
-		floatanim.play("treasure")
 
 func _on_body_entered(body):
 	if body.name == "Player": #Detects if Player enters the endpoint
