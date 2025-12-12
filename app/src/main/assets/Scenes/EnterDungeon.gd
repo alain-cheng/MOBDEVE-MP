6689bdbba9c6358be7569ed5eee258051a1475b7
jamchuy <104501078+Jamchuyz@users.noreply.github.com>
2025-12-12 15:37:38 +0800

    New Map , fixes

--- app/src/main/assets/Scenes/EnterDungeon.gd
+++ app/src/main/assets/Scenes/EnterDungeon.gd
@@ -2,8 +2,8 @@ extends Node
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	get_tree().change_scene_to_file("res://floors/Dungeon_A/1.tscn")
+#func _ready():
+	#get_tree().change_scene_to_file("res://floors/Dungeon_A/3.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
